f754dfd2566a8793e2b93130ba31bc9d667410db
Alexander Drager <szdrager@gmail.com>
2022-04-08 16:27:44 +0700

    Add OOP tests

---
+++ __tests__/OOP/OOPTaskEleven.test.js
@@ -0,0 +1,14 @@
+// @ts-check
+
+import ParseError from '../ParseError.js';
+import { parseJson } from '../json.js';
+
+test('testing parse json', () => {
+  const json = '{ "key": "value" }';
+  expect(parseJson(json)).toEqual({ key: 'value' });
+});
+
+test('testing parse invalid json', () => {
+  const json = '{ key": "value" }';
+  expect(() => parseJson(json)).toThrow(ParseError);
+});
